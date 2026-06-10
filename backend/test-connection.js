d9c62b484dd94fbb42c80ace5e31b0f952fb0ae1
Yathin10-sys <pyathin26@gmail.com>
2026-06-10 12:10:08 +0530

    Remove exposed MongoDB credentials

--- backend/test-connection.js
+++ backend/test-connection.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 console.log("Starting MongoDB connection test...");
 
-// Direct connection (non-SRV) - bypasses DNS SRV query issue
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
