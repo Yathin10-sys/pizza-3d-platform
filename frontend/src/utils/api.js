07e5652be577648118b042b17727f3bcfd2074a9
Yathin10-sys <pyathin26@gmail.com>
2026-06-10 11:55:25 +0530

    Remove exposed credentials

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 import { store } from '../redux/store';
 import { updateTokens, logout } from '../redux/slices/authSlice';
 
+const API_BASE = import.meta.env.VITE_API_URL || '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: API_BASE,
   headers: {
     'Content-Type': 'application/json'
   }
